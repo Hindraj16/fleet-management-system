269db25c6754e0c4efe494e73ada305758ba9131
Hindraj16 <kolihindraj21@gmail.com>
2026-09-19 11:46:09 +0530

    update urls.py

--- backend/fleet/urls.py
+++ backend/fleet/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import VehicleViewSet, GPSLocationViewSet
+from .views import VehicleViewSet, GPSLocationViewSet, VehicleRouteViewSet, VehicleRouteHistoryViewSet 
 
 router = DefaultRouter()
 
 router.register(r"vehicles", VehicleViewSet, basename="vehicle")
 router.register(r"gps-locations", GPSLocationViewSet, basename="gps-location")
+router.register(r'assigned-routes', VehicleRouteViewSet, basename='assigned-route')
+router.register(r'route-history', VehicleRouteHistoryViewSet, basename='route-history')
 
 urlpatterns = router.urls
