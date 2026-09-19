b018cb56a340ef6412af6a63699690c245b2ad65
Hindraj16 <kolihindraj21@gmail.com>
2026-09-19 11:17:11 +0530

    update models.py

--- backend/fleet/models.py
+++ backend/fleet/models.py
@@ -6,7 +6,7 @@ def generate_api_key():
 
 class Vehicle(models.Model):
     vehicle_number = models.CharField(max_length=50, unique=True)
-    device_id = models.CharField(max_length=100, unique=True)
+    device_id = models.CharField(max_length=100, blank=True, null=True)
     vendor_name = models.CharField(max_length=100, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,3 +25,29 @@ class GPSLocation(models.Model):
 
     class Meta:
         ordering = ['-gps_timestamp']
+
+class UploadRouteHistory(models.Model):
+    vehicle = models.ForeignKey("Vehicle",on_delete=models.CASCADE, related_name="route_histories")
+    route_name = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255, blank=True, null=True)
+    coordinates = models.JSONField(help_text="List of [lat, lng] pairs")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.route_name} ({len(self.coordinates)} points)"
+
+
+class UploadRoute(models.Model):
+    vehicle = models.ForeignKey("Vehicle",on_delete=models.CASCADE, related_name="uploaded_routes")
+    route_name = models.CharField(max_length=255, blank=True, null=True)
+    file = models.FileField(upload_to='kml_files/', blank=True, null=True)
+    coordinates = models.JSONField(blank=True, null=True) 
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        filename = self.file.name if self.file else "No file"
+        return f"{filename} - {self.route_name or 'Unnamed Route'}" 
