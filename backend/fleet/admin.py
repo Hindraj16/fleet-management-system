86c9c132c993670214e4b6ca107b25bec30200c2
Hindraj16 <kolihindraj21@gmail.com>
2026-09-19 11:35:55 +0530

    update admin.py

--- backend/fleet/admin.py
+++ backend/fleet/admin.py
@@ -1,7 +1,7 @@
 # fleet/admin.py
 
 from django.contrib import admin
-from .models import Vehicle, GPSLocation
+from .models import Vehicle, GPSLocation, UploadRouteHistory, UploadRoute
 
 
 @admin.register(Vehicle)
@@ -33,3 +33,18 @@ class GPSLocationAdmin(admin.ModelAdmin):
     )
     list_filter = ('ignition', 'vehicle')
     search_fields = ('vehicle__vehicle_number',)
+
+    
+@admin.register(UploadRouteHistory)
+class UploadRouteHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vehicle', 'file_name', 'route_name', 'coordinates', 'created_at',)
+    list_filter = ('file_name',)
+    search_fields = ('route_name',)
+    
+
+@admin.register(UploadRoute)
+class UploadRouteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vehicle', 'file', 'route_name', 'coordinates', 'assigned_at','updated_at')
+    list_filter = ('file',)
+    search_fields = ('file', 'route_name',)
+     
